b6f1112efe00c4fbfa8d7cd852ef84430d676433
tufusa <tufusa74@gmail.com>
2024-12-12 19:43:23 +0900

    feat: エラーハンドリング

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,53 @@
-import * as vscode from "vscode";
+import vscode from "vscode";
 
-const compilerUrl = "https://ceres.epi.it.matsue-ct.ac.jp/compile";
+const compilerUrl = "https://ceres.epi.it.matsue-ct.ac.jp/compil";
 const writerUrl = "https://ceres.epi.it.matsue-ct.ac.jp/writer";
+
+type PostCodeRequest = {
+  code: string;
+};
+
+type PostCodeResponse = {
+  status: string;
+  id: string;
+};
 
 export const activate = (context: vscode.ExtensionContext) => {
   const disposable = vscode.commands.registerCommand(
     "kaniwriter-vscode.open-kaniwriter",
     async () => {
-      vscode.window.showInformationMessage("Hello World from Kaniwriter!");
-      const editor = vscode.window.activeTextEditor;
-      const code = editor?.document.getText();
-      if (!code) return;
+      try {
+        const editor = vscode.window.activeTextEditor;
+        const code = editor?.document.getText();
+        if (!code) return;
 
-      const codeUploadRes = await fetch(`${compilerUrl}/code`, {
-        method: "POST",
-        body: JSON.stringify({ code: Buffer.from(code).toString("base64") }),
-      });
-      const id = ((await codeUploadRes.json()) as { id: string }).id;
-      vscode.window.showInformationMessage(code ?? "");
-      vscode.env.openExternal(vscode.Uri.parse(`${writerUrl}/?id=${id}`));
+        const uploadResponse = await fetch(`${compilerUrl}/code`, {
+          method: "POST",
+          body: JSON.stringify({
+            code: Buffer.from(code).toString("base64"),
+          } satisfies PostCodeRequest),
+        });
+        if (!uploadResponse.ok) {
+          throw new Error("Failed to fetch kaniwriter", {
+            cause: uploadResponse,
+          });
+        }
+
+        const uploadResult = (await uploadResponse.json()) as PostCodeResponse;
+        vscode.env.openExternal(
+          vscode.Uri.parse(`${writerUrl}/?id=${uploadResult}`)
+        );
+      } catch (err) {
+        console.error(err);
+        const select = await vscode.window.showErrorMessage(
+          "Failed to upload the program to kaniwriter",
+          "Retry",
+          "Close"
+        );
+        if (select === "Retry") {
+          vscode.commands.executeCommand("kaniwriter-vscode.open-kaniwriter");
+        }
+      }
     }
   );
 
